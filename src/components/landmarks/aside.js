07d3040ef1645ffdcb0d3b81a3378c6ffbcaa40e
Noelia Ruiz Martínez <nrm1977@gmail.com>
2022-09-08 09:55:10 +0200

    Improve links

--- src/components/landmarks/aside.js
+++ src/components/landmarks/aside.js
@@ -4,9 +4,12 @@ const Aside = () => {
 		<aside aria-labelledby="asideAccessibilityInfo">
 		<h2 id="asideAccessibilityInfo">Información adicional sobre accesibilidad</h2>
 		<ul>
-		<li><a href="https://www.w3.org/WAI" hrefLang="en"><abbr title="Web Accessibility Initiative">WAI</abbr> (abre nueva ventana)</a></li>
-		<li> <a href="https://daisy.org">Consorcio <abbr title="Digital Accessible Information System">DAISY</abbr></a></li>
-		<li> <a href="https://edrlab.org"><abbr title="European Digital Reading Lab">EDRLab</abbr></a></li>
+		<li><a href="https://www.w3.org/WAI" hrefLang="en" target="_blank"
+		rel="noopener noreferrer"><abbr title="Web Accessibility Initiative">WAI</abbr> (abre nueva ventana)</a></li>
+		<li> <a href="https://daisy.org" target="_blank"
+		rel="noopener noreferrer">Consorcio <abbr title="Digital Accessible Information System">DAISY</abbr> (abre nueva ventana)</a></li>
+		<li> <a href="https://edrlab.org" target="_blank"
+		rel="noopener noreferrer"><abbr title="European Digital Reading Lab">EDRLab</abbr> (abre nueva ventana)</a></li>
 		</ul>
 		</aside>
 		</>
--- src/components/landmarks/header.js
+++ src/components/landmarks/header.js
@@ -8,7 +8,7 @@ const Header = () => {
 		<p>Esta web proporciona información y elementos interactivos para comprobar el efecto de cumplir con ciertos criterios de las <abbr title="Web Content Accessibility Guidelines">WCAG</abbr> 2.1.</p>
 		<p>Se trata de un proyecto personal que no está apoyado por ninguna entidad y puede contener información errónea o imprecisa. Si crees que puede mejorarse, usa alguno de los medios de contacto proporcionados al final de esta página para dejar tus comentarios.</p>
 		<p>Las <a href="https://www.w3.org/TR/WCAG21"><abbr title="Web Content Accessibility Guidelines">WCAG</abbr> 2.1 (abre nueva ventana)</a> son una recomendación del <abbr title="World Wide Web Consortium">W3C</abbr> y pueden ser utilizadas como material de referencia o citadas en otros documentos.</p>
-		<p>Además de estas pautas, se han consultado otras publicaciones del <abbr title="World Wide Web Consortium">W3C</abbr>, como queda reflejado en los enlaces de esta web.</p>
+		<p>Además de estas pautas, se han consultado otras publicaciones del <abbr title="World Wide Web Consortium">W3C</abbr>, como queda reflejado en los enlaces de esta web, y la <ExternalLink href="https://www.etsi.org/deliver/etsi_en/301500_301599/301549/03.02.01_60/en_301549v030201p.pdf" contents="EN 301 549 V3.2.1 en inglés" />.</p>
 		<p>Para empezar, consulta información y descarga <abbr title="Non Visual Desktop Access">NVDA</abbr> desde la <ExternalLink href="https://www.nvaccess.org" contents="web de NV Access" />.</p>
 		</header>
 		</>
