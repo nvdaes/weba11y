7ca7a3275b1fd626451c59e91dedbfe60414604c
Noelia Ruiz Martínez <nrm1977@gmail.com>
2022-09-08 22:07:02 +0200

    Add contact form

--- src/components/landmarks/footer.js
+++ src/components/landmarks/footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
 		<section>
 		<h2 id="footer">Medios de contacto</h2>
 		<ul>
-		<li>formulario anónimo de Google</li>
+		<li><ExternalLink href="https://docs.google.com/forms/d/e/1FAIpQLScw-5o7SV8p53fhDtT02IPlRP7nwySPheQAy86zR-FVaV2REQ/viewform" hrefLang="es" contents="formulario anónimo de Google" /></li>
 		<li>correo electrónico: nrm1977@gmail.com</li>
 		<li>Twitter: @norrumar</li>
 		</ul>
