53392aa2d6b6cdd557b18215a76c179027be80eb
Noelia Ruiz Martínez <nrm1977@gmail.com>
2022-09-08 19:10:11 +0200

    Add last update

--- src/components/landmarks/footer.js
+++ src/components/landmarks/footer.js
@@ -1,16 +1,25 @@
 import ExternalLink from '../single/externalLink';
 
 const Footer = () => {
+	const update = new Date(document.lastModified);
+	const day = update.getDate();
+	const month = update.getMonth();
+	const year = update.getFullYear();
+
 	return (
 		<>
 		<footer>
+		<section>
 		<h2 id="footer">Medios de contacto</h2>
 		<ul>
 		<li>formulario anónimo de Google</li>
 		<li>correo electrónico: nrm1977@gmail.com</li>
 		<li>Twitter: @norrumar</li>
 		</ul>
+		</section>
 		<p><ExternalLink href="https://github.com/nvdaes/weba11y/" contents="Código fuente en GitHub" /></p>
+		<p>Última actualización: {day}/{month}/{year}</p>
+		
 		</footer>
 		</>
 	);
