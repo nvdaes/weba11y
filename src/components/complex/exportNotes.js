a941711dbbd78dcbc00397542c8d364dda487ddb
Noelia Ruiz Martínez <nrm1977@gmail.com>
2022-09-16 10:23:33 +0200

    Improve import notes and remove ability to delete them

--- src/components/complex/exportNotes.js
+++ src/components/complex/exportNotes.js
@@ -2,61 +2,62 @@ import { useState } from 'react';
 
 import ModalDialog from '../single/modalDialog';
 
-const DownloadLink = () =>{
-	const [loaded, setLoaded] = useState("");
+const Dialog = () =>{
+	const [outputText, setOutputText] = useState("");
 	const [fileData, setFileData] = useState("");
+
 	const handleChange = (e) => {
 		const fileReader = new FileReader();
 		fileReader.onload = async () => {
 		await setFileData(fileReader.result);
-		await setLoaded("Cargado");
+		await setOutputText("Cargado");
 	}
 		fileReader.readAsText(e.target.files[0]);
 	}
 
-	const handleClick= (e) => {
-		localStorage.clear();
-	}
-
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		const json = JSON.parse(fileData);
-		Object.entries(json).forEach(([key, value]) => {
-			localStorage.setItem(key, value);
-		});
+		try {
+			const json = JSON.parse(fileData);
+			Object.entries(json).forEach(([key, value]) => {
+				localStorage.setItem(key, value);
+			});
+			setOutputText("Notas importadas. Podrás ver las nuevas notas cuando recargues la página.");
+		} catch(err) {
+			setOutputText(err);
+		}
 	}
 
 	return (
-	<>
-<a download="nvda.json" href={`data:application/json;charset=utf-8,${encodeURIComponent(JSON.stringify(localStorage))}`}>Descargar notas</a>
-<form onSubmit={handleSubmit}>
-<label>Archivo de notas:
-<input type="file" accept=".json" required onChange={handleChange} />
-</label>
-<output>{loaded}</output>
-<input type="submit" value="Importar notas" />
-<label>Borrar notas
-<input type="button" onClick={handleClick} />
-</label>
-</form>
-</>
-
+		<>
+		<a download="nvda.json" href={`data:application/json;charset=utf-8,${encodeURIComponent(JSON.stringify(localStorage))}`}>Descargar notas</a>
+		<form onSubmit={handleSubmit}>
+		<label>Archivo de notas para importar:
+		<input type="file" accept=".json" required onChange={handleChange} />
+		</label>
+		<output>{outputText}</output>
+		<input type="submit" value="Importar notas" />
+		</form>
+		</>
 	)
 }
 
 const ExportNotes = () => {
 	const [isOpen, setIsOpen] = useState(false);
+
 	const handleClick = () => {
 		setIsOpen(true);
 	}
+
 	const closeModal = () => {
 		setIsOpen(false);
 	}
+
 	return (
-	<>
-	<button onClick={handleClick}>Administrar notas</button>
-	<ModalDialog isOpen={isOpen} closeModal={closeModal} contentLabel='Administrar notas' content={<DownloadLink />} />
-	</>
+		<>
+		<button onClick={handleClick}>Administrar notas</button>
+		<ModalDialog isOpen={isOpen} closeModal={closeModal} contentLabel='Administrar notas' content={<Dialog />} />
+		</>
 	)
 }
 
