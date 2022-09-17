4853434c9307c2b305de50e6bce45f4591b23d0d
Noelia Ruiz Martínez <nrm1977@gmail.com>
2022-09-17 05:21:46 +0200

    Use address in footer

--- src/components/landmarks/footer.js
+++ src/components/landmarks/footer.js
@@ -6,14 +6,16 @@ const Footer = () => {
 		<footer>
 		<section>
 		<h2 id="footer">Medios de contacto</h2>
+		<address>
 		<ul>
 		<li><ExternalLink href="https://docs.google.com/forms/d/e/1FAIpQLScw-5o7SV8p53fhDtT02IPlRP7nwySPheQAy86zR-FVaV2REQ/viewform" hrefLang="es" contents="formulario anónimo de Google" /></li>
 		<li>correo electrónico: nrm1977@gmail.com</li>
 		<li>Twitter: @norrumar</li>
 		</ul>
+		</address>
 		</section>
 		<p><ExternalLink href="https://github.com/nvdaes/weba11y/" contents="Código fuente en GitHub" /></p>
-<p>Última actualización: <time datetime="2022-09-16">16 de septiembre de 2022</time></p>
+<p>Última actualización: <time datetime="2022-09-17">16 de septiembre de 2022</time></p>
 		</footer>
 		</>
 	);
